960df12befee7d89f1ffc1ce63611be652f2847f
yayo <jorgefloresari@gmail.com>
2020-08-10 23:12:47 +0000

    tutorial 2 rota suelo, voltea,  agrega movimiento adelante y atras

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ var scene, camera, renderer, mesh;
 var meshFloor;
 
 var keyboard = {};
-var player = {height:1.8};
+var player = {height:1.8, speed:0.2};
 
 function init()
 {
@@ -17,9 +17,10 @@ function init()
 	scene.add(mesh);
 
 	meshFloor = new THREE.Mesh(
-		new THREE.PlaneGeometry(10,10),
+		new THREE.PlaneGeometry(10,10,10,10),
 		new THREE.MeshBasicMaterial({color:0xffffff,wireframe:true})
 	);
+	meshFloor.rotation.x -= Math.PI/2;
 	scene.add(meshFloor);
 
 	camera.position.set(0,player.height,-5);
@@ -39,6 +40,15 @@ function animate()
 	mesh.rotation.x += 0.01;
 	mesh.rotation.y += 0.02;
 	
+	if(keyboard[87]){//w key
+		camera.position.x += Math.sin(camera.rotation.y)*player.speed;
+		camera.position.z += Math.cos(camera.rotation.y)*player.speed;
+	}
+	if(keyboard[83]){//s key
+		camera.position.x -= Math.sin(camera.rotation.y)*player.speed;
+		camera.position.z -= Math.cos(camera.rotation.y)*player.speed;
+	}
+
 	if(keyboard[37]){ //left arrow key
 		camera.rotation.y -= Math.PI * 0.01;
 	}
