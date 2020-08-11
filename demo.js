5a27aecffab9d93578c1811f7e59a4ed3204eb32
yayo <jorgefloresari@gmail.com>
2020-08-10 23:51:58 -0500

    tutorial 3 agrega luz

--- demo.js
+++ demo.js
@@ -11,7 +11,7 @@ function init()
 
 	mesh = new THREE.Mesh(
 		new THREE.BoxGeometry(1,1,1),
-		new THREE.MeshBasicMaterial({color:0xff9999, wireframe:false})
+		new THREE.MeshPhongMaterial({color:0xff9999, wireframe:false})
 	);
 
 	mesh.position.y += 1;
@@ -19,16 +19,30 @@ function init()
 
 	meshFloor = new THREE.Mesh(
 		new THREE.PlaneGeometry(10,10,10,10),
-		new THREE.MeshBasicMaterial({color:0xffffff,wireframe:false})
+		new THREE.MeshPhongMaterial({color:0xffffff,wireframe:false})
 	);
 	meshFloor.rotation.x -= Math.PI/2;
 	scene.add(meshFloor);
+
+	ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+	scene.add(ambientLight);
+
+	//luz
+	light = new THREE.PointLight(0xffffff, 0.8 , 18);
+	light.position.set(-3,6,-3);
+	light.castShadow = true;
+	light.shadow.camera.near = 0.1;
+	light.shadow.camera.far = 25;
+	scene.add(light);
 
 	camera.position.set(0,player.height,-5);
 	camera.lookAt(new THREE.Vector3(0,player.height,0));
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(1200,720);
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.BasicShadowMap;
+
 	document.body.appendChild(renderer.domElement);
 
 	animate();
